74929b7a477bf360477cbff3469fd5b973f711c7
mic006016@gmail.com <mic006016@gmail.com>
2025-09-02 17:30:43 +0900

    BookForm

--- src/components/book/BookForm.jsx
+++ src/components/book/BookForm.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react"
 import styled from "@emotion/styled"
-import { Button } from "@mui/material"
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material"
 
 const FormWrap = styled.form`
   display: flex;
@@ -15,11 +21,57 @@ const Input = styled.input`
   flex-grow: ${(props) => props.grow};
 `
 export default function BookForm() {
+  const [form, setForm] = useState({ title: "", content: "" })
+  const [isErrOpen, setIsErrOpen] = useState(false)
+  const [errMsg, setErrMsg] = useState("")
+  const onSubmit = (e) => {
+    if (form.title === "") {
+      setIsErrOpen(true)
+      setErrMsg("제목은 필수사항입니다.")
+      return
+    }
+    if (form.content === "") {
+      setIsErrOpen(true)
+      setErrMsg("내용은 필수사항입니다.")
+      return
+    }
+    // TODO :: 전송
+  }
+  const handleClose = (e) => {
+    setIsErrOpen(false)
+    setErrMsg("")
+  }
   return (
     <FormWrap>
-      <Input placeholder="제목" grow={1} />
-      <Input placeholder="설명" grow={3} />
-      <Button varient="contained">등록</Button>
+      <Dialog open={isErrOpen} onClose={handleClose}>
+        <DialogContent>
+          <DialogContentText>{errMsg}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            확인
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Input
+        placeholder="제목"
+        grow={1}
+        value={form.title}
+        onChange={(e) =>
+          setForm((prev) => ({ ...prev, title: e.target.value }))
+        }
+      />
+      <Input
+        placeholder="설명"
+        grow={3}
+        value={form.content}
+        onChange={(e) =>
+          setForm((prev) => ({ ...prev, content: e.target.value }))
+        }
+      />
+      <Button variant="contained" onClick={onSubmit}>
+        등록
+      </Button>
     </FormWrap>
   )
 }
